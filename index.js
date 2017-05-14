e9b9e06fcf4f3d99798d3e68a32e208162d45b24
Pepe Becker <mail@pepebecker.com>
2017-05-14 15:03:57 +0200

    Update to use new version of find-hanzi

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 'use strict'
 
 const findHanzi = require('find-hanzi')
+const so = require('so')
 
 const convert = (text) => new Promise((yay, nay) => {
-	findHanzi(text, {multiple: true}).then((data) => {
-		const result = data.map((item) => item.pinyin)
-		yay(result.join(' '))
-	}, (error) => nay('hanzi-to-pinyin -> ' + error))
+	so(function*() {
+		let list = []
+		const characters = text.split('')
+		for (let hanzi of characters) {
+			yield findHanzi(hanzi).then((data) => {
+				const pinyin = data.map((item) => item.pinyin[0])[0]
+				list.push(pinyin)
+			}, (error) => nay('hanzi-to-pinyin -> ' + error))
+		}
+		if (list.length > 0) {
+			yay(list.join(' '))
+			return
+		}
+		nay('error')
+	})()
 })
 
 module.exports = convert
--- test/index.js
+++ test/index.js
@@ -6,6 +6,6 @@ describe('Convert', () => {
 	it('should convert characters to Pinyin', () => {
 		return convert('我的猫喜欢喝牛奶').then((data) => {
 			data.should.equal('wǒ de māo xǐ huān hē niú nǎi')
-		})
+		}).catch(console.error)
 	})
 })
