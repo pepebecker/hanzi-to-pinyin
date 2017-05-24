4f62c933d984d47aed09ad309199dd3ff2567d17
Pepe Becker <mail@pepebecker.com>
2017-05-24 22:26:53 +0200

    Add support for special characters

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ const convert = (text) => new Promise((yay, nay) => {
 	so(function*() {
 		let list = []
 		const characters = text.split('')
-		for (let hanzi of characters) {
-			yield findHanzi(hanzi).then((data) => {
-				const pinyin = data.map((item) => item.pinyin[0])[0]
-				list.push(pinyin)
-			}, (error) => nay('hanzi-to-pinyin -> ' + error))
+		for (let char of characters) {
+			yield findHanzi(char).then((data) => {
+				if (data.length > 0) {
+					const pinyin = data.map((item) => item.pinyin[0])[0]
+					list.push(pinyin)
+				} else {
+					list.push(char)
+				}
+			}, (error) => nay(error))
 		}
 		if (list.length > 0) {
 			yay(list.join(' '))
