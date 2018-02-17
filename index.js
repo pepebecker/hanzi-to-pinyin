a8bbaa153abfcf0dc5ed65b5780dc5ff7d5d6e20
Pepe Becker <mail@pepebecker.com>
2018-02-17 08:56:46 +0100

    Use mdbg instead of find-hanzi

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 'use strict'
 
-const findHanzi = require('find-hanzi')
-const so = require('so')
+const mdbg = require('mdbg')
 
-const convert = (text) => new Promise((yay, nay) => {
-	so(function*() {
-		let list = []
-		const characters = text.split('')
-		for (let char of characters) {
-			yield findHanzi(char).then((data) => {
-				if (data.length > 0) {
-					const pinyin = data.map((item) => item.pinyin[0])[0]
-					list.push(pinyin)
-				} else {
-					list.push(char)
-				}
-			}, (error) => nay(error))
+const getTokens = async text => {
+	let list = []
+	let index = 0
+	while (index < text.length) {
+		let word = text.substr(index)
+		let count = word.length
+		let wordFound = false
+		while (count >= 0) {
+			word = word.substr(0, count)
+			const entry = mdbg.get(word)
+			if (entry) {
+				index += count - 1
+				entry.word = word
+				list.push(entry)
+				wordFound = true
+				break
+			}
+			count--
 		}
-		if (list.length > 0) {
-			yay(list.join(' '))
-			return
+		if (!wordFound) {
+			list.push(text[index])
 		}
-		nay('error')
-	})()
-})
+		index++
+	}
+	return list
+}
+
+const convert = async text => {
+	const tokens = await getTokens(text)
+	return tokens.map(token => {
+		let keys = Object.keys(token.data)
+		return keys.map(key => token.data[key].mandarin)
+	})
+}
 
 module.exports = convert
--- test/index.js
+++ test/index.js
@@ -4,8 +4,14 @@ const convert = require('../index')
 
 describe('Convert', () => {
 	it('should convert characters to Pinyin', () => {
-		return convert('我的猫喜欢喝牛奶').then((data) => {
-			data.should.equal('wǒ de māo xǐ huān hē niú nǎi')
-		}).catch(console.error)
+		return convert('我的猫喜欢喝牛奶').then(data => {
+			data.map(l => {
+				if (l.length > 1) {
+					return '[' + l.join(',') + ']'
+				} else {
+					return l[0]
+				}
+			}).join(' ').should.equal('wǒ [de,dī,dí,dì] māo xǐ huan [hē,hè] niú nǎi')
+		})
 	})
 })
